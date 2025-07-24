d56dfd2922d7e510524f53742e7f721ca0f2da0a
mgalcuadrado <mgamberale@fi.uba.ar>
2025-07-24 16:08:52 -0300

    TDA potenciómetro creado

---
+++ tdse-tpf_2_09/app/inc/potenciometro.h
@@ -0,0 +1,21 @@
+/*
+ * potenciometro.h
+ *
+ *  Created on: Jul 24, 2025
+ *      Author: mechi
+ */
+
+#ifndef INC_POTENCIOMETRO_H_
+#define INC_POTENCIOMETRO_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+void potenciometro_init(void);
+void potenciometro_update(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* INC_POTENCIOMETRO_H_ */
